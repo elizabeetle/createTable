03bd754738b00202dd8d40888a29fb46315f0b58
elizabeetle <89229500+elizabeetle@users.noreply.github.com>
2021-11-29 15:43:30 -0500

    Add files via upload
    Many changes have been made. Chages to class names, functionality, and other small changes. Future updates will be smaller and better documented.

---
+++ EnterData.java
@@ -0,0 +1,113 @@
+package createPDF;
+
+import java.util.Scanner;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+public class EnterData{
+	
+	Scanner scan = new Scanner(System.in);
+	String term;
+	String definition;
+	String syntax;
+	String answer;
+	String answerdataCheck;
+	Multimap<String, String> hashmapData = HashMultimap.create();
+	
+	
+
+//	the above seems wrong? is this another copy of invokeDataEntry in a different place? or are they the same being shared
+//		in the two different places?
+
+	public void dataEntry() {
+	
+		
+		System.out.println("\nEnter the command: " );
+	    term = scan.nextLine();
+	    
+	    System.out.println("\nEnter the use/definition: ");
+	    definition = scan.nextLine();
+	    
+	    System.out.println("\nEnter the syntax: ");
+	    syntax = scan.nextLine();
+	    
+//	    EnterData enterData = new EnterData();
+//		was recommended to make a new instance of EnterData to fix the static issue
+		dataCheck();
+		//don't need to instantatite, define logic
+		
+		
+	}
+	
+	public void dataCheck() {
+		
+		System.out.println("Is this information correct?" + "\n\n" + term + "---" + definition + "---" + syntax + " \n");
+	    answerdataCheck = scan.nextLine();
+	
+	    
+	//line 50 to 70 turn into do - while loop (might have to take pieces apart)
+	if(answerdataCheck.charAt(0) == 'Y' || answerdataCheck.charAt(0) == 'y') {
+			//this makes it so that if the first character is 'y' or 'Y' it will execute
+			//this is wrong... uppercase Y doesn't work...
+			hashmapData.put(term, definition);
+			hashmapData.put(term, syntax);
+		
+			
+			System.out.println(hashmapData);
+			//this is where the PDF template entry needs to go...
+			
+			anotherEntry();
+
+
+		} else if(answerdataCheck.charAt(0) == ('N' | 'n')) {
+			System.out.println("Please re-enter the information...\n");
+			dataEntry();
+			
+		} else {
+		    dataCheck();
+		    //recursion, try to avoid
+		    //while loop; would get rid of recursion
+	
+		}
+
+
+		
+	} 
+	
+	public void anotherEntry() {
+		
+		
+		System.out.println("\nWould you like to make another entry?");
+	    answer = scan.nextLine();
+	    
+		
+		if(answer.charAt(0) == 'Y' || answer.charAt(0) == 'y') {
+//		can leave blank because the "answer" variable is "yes" and will 
+//			automatically take you back to dataEntry via the "while" section of the loop in scratch 
+			
+		
+		} else if(answer.charAt(0) == ('N' | 'n')) {
+			System.out.println("\nYour document has been created. Check the current work folder for a PDF file.\n");
+			
+		} else {
+			anotherEntry();
+		
+		}
+	}
+
+
+	
+	public void closeScanner() {
+		
+		scan.close();
+	}
+
+		
+	
+	
+//	System.out.println("What would you like to name this PDF table?");
+//	String documentName = scan.nextLine();
+// the above needs to go at the start of the program
+
+}
